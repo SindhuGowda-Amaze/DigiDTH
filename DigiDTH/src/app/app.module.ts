b9a216264c2fe09436f0c454f7806dcfc31dbee2
Sindhu Gowda <SindhuGowda.amazeInc@gmail.com>
2022-04-01 11:32:45 +0530

    second push

--- DigiDTH/src/app/app.module.ts
+++ DigiDTH/src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgxDropzoneModule } from 'ngx-dropzone';
 
 @NgModule({
   declarations: [
@@ -10,6 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    NgxDropzoneModule,
     AppRoutingModule
   ],
   providers: [],
