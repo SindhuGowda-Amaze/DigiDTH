3ec31f21ecb5fd55b5d168125ff473a7fae16bd5
Anusha <anushavalmiki.amaze@gmail.com>
2022-04-01 12:24:10 +0530

    routing

--- DigiDTH/src/app/app-routing.module.ts
+++ DigiDTH/src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import {HeaderComponent} from './header/header.component'
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { CompanySetupComponent } from './company-setup/company-setup.component';
+import { StaffSetupComponent } from './staff-setup/staff-setup.component';
+import { CustomerReportComponent } from './customer-report/customer-report.component';
+import { NewInstallationComponent } from './new-installation/new-installation.component';
+import { CustomerIssuesComponent } from './customer-issues/customer-issues.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+
+  { path: '', redirectTo: '/Login', pathMatch: 'full' },
+  {path:'Login',component:LoginComponent},
+  {path:'Header',component:HeaderComponent},
+  {path:'Sidebar',component:SidebarComponent},
+  {path:'CompanySetup',component:CompanySetupComponent},
+  {path:'StaffSetup',component:StaffSetupComponent},
+  {path:'CustomerReport',component:CustomerReportComponent},
+  {path:'NewInstallation',component:NewInstallationComponent},
+  {path:'CustomerIssues',component:CustomerIssuesComponent}
+
+
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
