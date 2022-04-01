078ff4b613c7d2567b8f73cf2b8267c94d74510a
Sindhu Gowda <SindhuGowda.amazeInc@gmail.com>
2022-04-01 15:50:22 +0530

    ss

--- DigiDTH/src/app/login/login.component.ts
+++ DigiDTH/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,9 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  router: any;
+ 
 
-  constructor() { }
+  constructor(public router:Router) { }
 
   ngOnInit(): void {
     this.showpassword = 0;
